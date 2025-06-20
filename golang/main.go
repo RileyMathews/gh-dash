65f0c6834e0bdaf8f5ff0b85ccafd265f83aa8e8
Riley Mathews <rileymathews80@gmail.com>
2025-06-20 10:03:52 -0400

    now does basic filtering on my own Prs

--- golang/main.go
+++ golang/main.go
@@ -1,87 +1,141 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "sync"
+	"fmt"
+	"os"
+	"sync"
 )
 
 // Search result types (unchanged)
 type SearchPullRequestInfo struct {
-    URL string `json:"url"`
+	URL string `json:"url"`
 }
 type SearchItem struct {
-    PullRequest SearchPullRequestInfo `json:"pull_request"`
+	PullRequest SearchPullRequestInfo `json:"pull_request"`
 }
 type SearchIssuesResult struct {
-    Items []SearchItem `json:"items"`
+	Items []SearchItem `json:"items"`
 }
 
 func fetchPR(url string, idx int, prs []PullRequest, wg *sync.WaitGroup) {
-    defer wg.Done()
-    var prDetail PrDetail
-    if err := fetchGH(url, nil, &prDetail); err != nil {
-        fmt.Fprintf(os.Stderr, "Error fetching PR details: %v\n", err)
-        os.Exit(1)
-    }
+	defer wg.Done()
+	var prDetail PrDetail
+	if err := fetchGH(url, nil, &prDetail); err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching PR details: %v\n", err)
+		os.Exit(1)
+	}
 
-    // 3) Fetch comments
-    var comments []Comment
-    if err := fetchGH(prDetail.CommentsURL, nil, &comments); err != nil {
-        fmt.Fprintf(os.Stderr, "Error fetching comments: %v\n", err)
-        os.Exit(1)
-    }
+	// 3) Fetch comments
+	var comments []Comment
+	if err := fetchGH(prDetail.CommentsURL, nil, &comments); err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching comments: %v\n", err)
+		os.Exit(1)
+	}
 
-    // 4) Fetch commits
-    var commits []PrCommit
-    if err := fetchGH(prDetail.CommitsURL, nil, &commits); err != nil {
-        fmt.Fprintf(os.Stderr, "Error fetching commits: %v\n", err)
-        os.Exit(1)
-    }
+	// 4) Fetch commits
+	var commits []PrCommit
+	if err := fetchGH(prDetail.CommitsURL, nil, &commits); err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching commits: %v\n", err)
+		os.Exit(1)
+	}
 
-    fullPR := PullRequest{
-        Pr:       prDetail,
-        Comments: comments,
-        Commits:  commits,
-    }
+	fullPR := PullRequest{
+		Pr:       prDetail,
+		Comments: comments,
+		Commits:  commits,
+	}
 
-    prs[idx] = fullPR
+	prs[idx] = fullPR
 }
 
 func fetchAllPRs(searchResult SearchIssuesResult) []PullRequest {
-    n := len(searchResult.Items)
-    prs := make([]PullRequest, n)
-    var wg sync.WaitGroup
-    wg.Add(n)
+	n := len(searchResult.Items)
+	prs := make([]PullRequest, n)
+	var wg sync.WaitGroup
+	wg.Add(n)
 
-    for i, prItem := range searchResult.Items {
-        go fetchPR(prItem.PullRequest.URL, i, prs, &wg)
-    }
+	for i, prItem := range searchResult.Items {
+		go fetchPR(prItem.PullRequest.URL, i, prs, &wg)
+	}
 
-    wg.Wait()
-    return prs
+	wg.Wait()
+	return prs
+}
+
+func getLastActionUser(pr PullRequest) string {
+	var newestComment *Comment
+	for _, comment := range pr.Comments {
+		if newestComment == nil || comment.CreatedAt.After(newestComment.CreatedAt) {
+			newestComment = &comment
+		}
+	}
+
+	var newestCommit *PrCommit
+	for _, commit := range pr.Commits {
+		if newestCommit == nil || commit.Commit.Author.Date.After(newestCommit.Commit.Author.Date) {
+			newestCommit = &commit
+		}
+	}
+
+	if newestComment != nil {
+		return newestComment.User.Login
+	}
+
+	if newestCommit != nil {
+		return newestCommit.Author.Login
+	}
+
+	return pr.Pr.User.Login
+}
+
+func sortPRs(prs []PullRequest) []Section {
+	var myPrsNeedAttention = Section{
+		Name:         "My Prs that need attention",
+		PullRequests: []PullRequest{},
+	}
+	var myPrsNoActionNeeded = Section{
+		Name:         "My Prs no action needed",
+		PullRequests: []PullRequest{},
+	}
+
+	for _, pr := range prs {
+		var lastActor = getLastActionUser(pr)
+		if pr.Pr.User.Login == "RileyMathews" {
+			if lastActor != "RileyMathews" {
+				myPrsNeedAttention.PullRequests = append(myPrsNeedAttention.PullRequests, pr)
+			} else {
+				myPrsNoActionNeeded.PullRequests = append(myPrsNoActionNeeded.PullRequests, pr)
+			}
+		}
+	}
+
+	return []Section{myPrsNeedAttention, myPrsNoActionNeeded}
 }
 
 func main() {
-    searchParams := map[string]string{
-        "q":        "is:pr is:open org:MercuryTechnologies author:@me",
-        "per_page": "10",
-    }
+	searchParams := map[string]string{
+		"q":        "is:pr is:open org:MercuryTechnologies author:@me",
+		"per_page": "10",
+	}
 
-    // 1) Fetch open PR listings directly into searchResult
-    var searchResult SearchIssuesResult
-    if err := fetchGH("/search/issues", searchParams, &searchResult); err != nil {
-        fmt.Fprintf(os.Stderr, "Error fetching PR list: %v\n", err)
-        os.Exit(1)
-    }
-    if len(searchResult.Items) == 0 {
-        os.Exit(0)
-    }
+	// 1) Fetch open PR listings directly into searchResult
+	var searchResult SearchIssuesResult
+	if err := fetchGH("/search/issues", searchParams, &searchResult); err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching PR list: %v\n", err)
+		os.Exit(1)
+	}
+	if len(searchResult.Items) == 0 {
+		os.Exit(0)
+	}
 
-    var prs = fetchAllPRs(searchResult)
+	var prs = fetchAllPRs(searchResult)
+	var sections = sortPRs(prs)
 
-    for _, details := range prs {
-        fmt.Printf("%+v\n", details)
-    }
+	for _, section := range sections {
+		fmt.Printf("%s\n", section.Name)
+		for _, pr := range section.PullRequests {
+			fmt.Printf("%s %s\n", pr.Pr.Title, pr.Pr.UiUrl)
+		}
+		fmt.Printf("\n")
+	}
 }
-
--- golang/models.go
+++ golang/models.go
@@ -6,63 +6,69 @@ import "time"
 
 // Basic user information
 type GithubUser struct {
-    Login string `json:"login"`
-    ID    int    `json:"id"`
-    Type  string `json:"type"`
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+	Type  string `json:"type"`
 }
 
 // Repository information (owner is a GithubUser)
 type GithubRepo struct {
-    ID       int       `json:"id"`
-    Name     string    `json:"name"`
-    FullName string    `json:"full_name"`
-    Private  bool      `json:"private"`
-    Owner    GithubUser `json:"owner"`
+	ID       int        `json:"id"`
+	Name     string     `json:"name"`
+	FullName string     `json:"full_name"`
+	Private  bool       `json:"private"`
+	Owner    GithubUser `json:"owner"`
 }
 
 // Base contains the repository info for a PR (part of PR details)
 type Base struct {
-    Repo GithubRepo `json:"repo"`
+	Repo GithubRepo `json:"repo"`
 }
 
 // Pull Request detail (basic PR info and links to related resources)
 type PrDetail struct {
-    Title       string    `json:"title"`
-    Number      int       `json:"number"`
-    Base        Base      `json:"base"`
-    CommitsURL  string    `json:"commits_url"`
-    CommentsURL string    `json:"comments_url"`
-    CreatedAt   time.Time `json:"created_at"`
+	Title       string     `json:"title"`
+	Number      int        `json:"number"`
+	Base        Base       `json:"base"`
+	CommitsURL  string     `json:"commits_url"`
+	CommentsURL string     `json:"comments_url"`
+	CreatedAt   time.Time  `json:"created_at"`
+	User        GithubUser `json:"user"`
+	UiUrl       string     `json:"issue_url"`
 }
 
 // Comment on a PR (we capture the author and creation time)
 type Comment struct {
-    User      GithubUser `json:"user"`
-    CreatedAt time.Time  `json:"created_at"`
+	User      GithubUser `json:"user"`
+	CreatedAt time.Time  `json:"created_at"`
 }
 
 // Commit author details (from the commit object in a PR commit)
 type CommitAuthor struct {
-    Name  string    `json:"name"`
-    Email string    `json:"email"`
-    Date  time.Time `json:"date"`
+	Name  string    `json:"name"`
+	Email string    `json:"email"`
+	Date  time.Time `json:"date"`
 }
 
 // Commit holds commit metadata (we include only the author sub-structure here)
 type Commit struct {
-    Author CommitAuthor `json:"author"`
+	Author CommitAuthor `json:"author"`
 }
 
 // PR commit entry, including the GitHub user (author) and the commit details
 type PrCommit struct {
-    Author GithubUser `json:"author"`
-    Commit Commit     `json:"commit"`
+	Author GithubUser `json:"author"`
+	Commit Commit     `json:"commit"`
 }
 
 // Aggregate PullRequest structure combining PR details, comments, and commits
 type PullRequest struct {
-    Pr       PrDetail   `json:"pr"`
-    Comments []Comment  `json:"comments"`
-    Commits  []PrCommit `json:"commits"`
+	Pr       PrDetail   `json:"pr"`
+	Comments []Comment  `json:"comments"`
+	Commits  []PrCommit `json:"commits"`
 }
 
+type Section struct {
+	Name         string
+	PullRequests []PullRequest
+}
